fdcf40ea044bfcca9bc15fba0a2d3466a71fe984
Andreius_14 <Andreius_14@outlook.com>
2023-08-28 00:45:19 -0500

    Mini-Proyectos-Prueba Codigos Rapidos

---
+++ Mini-Python/Hackerrank/script.js
@@ -0,0 +1,17 @@
+
+//// Sin operador ternario
+//let edad = 30;
+//let mensaje;
+//if (edad >= 18) {
+//  mensaje = "Eres mayor de edad";
+//} else {
+//  mensaje = "Eres menor de edad";
+//}
+//
+//// Con operador ternario
+//let edad = 30;
+//let mensaje = edad >= 18 ? "Mayor" : "Menor";
+
+
+
+  
